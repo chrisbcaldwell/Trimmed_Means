909e491050f5c1f4f72fc5b3a6406ca0c337437e
Chris Caldwell <chriscaldwell2026@u.northwestern.edu>
2025-06-06 09:57:39 -0700

    final version

--- go.mod
+++ go.mod
@@ -1,3 +1,13 @@
 module trimmed_means
 
 go 1.23.2
+
+require github.com/go-gota/gota v0.12.0
+
+require github.com/chrisbcaldwell/trim v0.0.2
+
+require (
+	golang.org/x/exp v0.0.0-20250606033433-dcc06ee1d476 // indirect
+	golang.org/x/net v0.0.0-20210423184538-5f58ad60dda6 // indirect
+	gonum.org/v1/gonum v0.9.1 // indirect
+)
--- main.go
+++ main.go
@@ -2,8 +2,47 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"time"
+
+	"github.com/chrisbcaldwell/trim"
+
+	"github.com/go-gota/gota/dataframe"
 )
 
+const filepath = "random.csv"
+const quantile = 0.05
+
 func main() {
-	fmt.Println("tacos")
+	start := time.Now()
+	df, err := readToDataFrame(filepath)
+	if err != nil {
+		fmt.Println("could not read", filepath)
+	}
+	df.SetNames("integers", "floats")
+
+	ints, err := df.Col("integers").Int()
+	if err != nil {
+		fmt.Println("Column 'integers' not of integer type")
+	}
+	floats := df.Col("floats").Float()
+
+	intTM := trim.TrimmedMean(ints, quantile)
+	floatTM := trim.TrimmedMean(floats, quantile)
+	elapsed := time.Since(start)
+	fmt.Printf("Integer data trimmed mean: %.2f\n", intTM)
+	fmt.Printf("Float data trimmed mean: %.2f\n", floatTM)
+	fmt.Println("Total run time:", elapsed)
+}
+
+func readToDataFrame(p string) (dataframe.DataFrame, error) {
+	f, err := os.Open(p)
+	if err != nil {
+		fmt.Println("Unable to read input file " + filepath)
+		return dataframe.New(), err
+	}
+	defer f.Close()
+
+	df := dataframe.ReadCSV(f)
+	return df, nil
 }
